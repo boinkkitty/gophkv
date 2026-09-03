00e79a654b6c6682ed264e846ecb40f53e3f1d24
boinkkitty <boinkitty@gmail.com>
2026-09-04 03:42:37 +0800

    feat: add tree walk interpreter

---
+++ internal/sql/eval.go
@@ -0,0 +1,52 @@
+package sql
+
+import (
+	"errors"
+	"slices"
+)
+
+// evalExpr evaluates an expression AST against a row using a tree-walk interpreter.
+func evalExpr(schema *Schema, row Row, expr interface{}) (*Cell, error) {
+	switch e := expr.(type) {
+	case string:
+		idx := slices.IndexFunc(schema.Cols, func(col Column) bool {
+			return col.Name == e
+		})
+		if idx < 0 {
+			return nil, errors.New("unknown column")
+		}
+		return &row[idx], nil
+	case *Cell:
+		return e, nil
+	case *ExprBinOp:
+		left, err := evalExpr(schema, row, e.left)
+		if err != nil {
+			return nil, err
+		}
+		right, err := evalExpr(schema, row, e.right)
+		if err != nil {
+			return nil, err
+		}
+		// Type checking
+		if left.Type != right.Type {
+			return nil, errors.New("binary op type mismatch")
+		}
+
+		out := &Cell{Type: left.Type}
+		switch {
+		//s string concat
+		case e.op == OP_ADD && out.Type == TypeStr:
+			out.Str = slices.Concat(left.Str, right.Str)
+		// arithmetic
+		case e.op == OP_ADD && out.Type == TypeI64:
+			out.I64 = left.I64 + right.I64
+		case e.op == OP_SUB && out.Type == TypeI64:
+			out.I64 = left.I64 - right.I64
+		default:
+			return nil, errors.New("bad binary op")
+		}
+		return out, nil
+	default:
+		panic("unreachable")
+	}
+}
